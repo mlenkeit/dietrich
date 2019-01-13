8b15c45535ab4d58884c745c6c3e409292893fe6
Maximilian Lenkeit <maximilian.lenkeit@sap.com>
2019-01-13 18:18:48 +0100

    feat(localtunnel): register localtunnel clients

--- server.js
+++ server.js
@@ -3,9 +3,14 @@
 const express = require('express')
 
 const auth = require('./lib/mw/auth')
+const localtunnel = require('./lib/router/localtunnel')
+const tunnels = require('./lib/tunnels')
 
 module.exports = function (opts) {
+  const tunnelHandler = tunnels()
+
   const app = express()
+  require('express-ws')(app)
 
   app.use(auth({
     apiToken: opts.api_token,
@@ -16,6 +21,11 @@ module.exports = function (opts) {
   app.get('/', (req, res) => {
     res.status(200).send('ok')
   })
+
+  // routers
+  app.get('/', localtunnel({
+    tunnelHandler: tunnelHandler
+  }))
 
   return app
 }
